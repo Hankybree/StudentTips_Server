776c2be2bccef6a824212af35b7591ccba222879
Henrik Frank <frank.havsvinge@gmail.com>
2020-05-20 13:07:21 +0200

    Added empty functions for GET, POST, PATCH and DELETE

--- server.js
+++ server.js
@@ -7,3 +7,19 @@ app.use(express.json())
 app.listen(12001, () => {
     console.log('Server running on port 12001')
 })
+
+app.get('/pins', (response, request) => {
+
+})
+
+app.post('/pins', (response, request) => {
+
+})
+
+app.patch('/pins/:pin', (response, request) => {
+
+})
+
+app.delete('/pins/:pin', (response, request) => {
+    
+})
