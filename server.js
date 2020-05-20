4812ce4c53f02670ce7ba51e70cec25e7db8e769
jakob salomonsson <jakob@salomonssons.se>
2020-05-20 14:57:12 +0200

    Implemented Patch function for server

--- server.js
+++ server.js
@@ -19,18 +19,40 @@ app.get('/pins', (request, response) => {
 
 app.post('/pins', (request, response) => {
 
-    request.body.id = uuidv4()
+    request.body.pinId = uuidv4()
     pins.push(request.body)
     response.send('object created')
 })
 
 app.patch('/pins/:pin', (request, response) => {
-
+    for (let i = 0; i < pins.length; i++) {
+        if (request.params.pin === pins[i].pinId) {
+            if (request.body.title !== undefined) {
+                pins[i].title = request.body.title
+            }
+            if (request.body.description !== undefined) {
+                pins[i].description = request.body.description
+            }
+            if (request.body.image !== undefined) {
+                pins[i].image = request.body.image
+            }
+            if (request.body.tags !== undefined) {
+                pins[i].tags = request.body.tags
+            }
+            if (request.body.coordinates !== undefined) {
+                pins[i].coordinates = request.body.coordinates
+            }
+            if (request.body.distance !== undefined) {
+                pins[i].distance = request.body.distance
+            }
+        }
+    }
+    response.send('Update complete')
 })
 
 app.delete('/pins/:pin', (request, response) => {
     for (let i = 0; i < pins.length; i++) {
-        if (request.params.pin === pins[i].id) {
+        if (request.params.pin === pins[i].pinId) {
             pins.splice(i, 1)
         }
     }
