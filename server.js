fd7590ea601fd5f2635871c7977c185bba565aff
Henrik Frank <frank.havsvinge@gmail.com>
2020-05-25 16:23:43 +0200

    Implemented PATCH

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ sqlite
     })
 
 app.get('/pins', (request, response) => {
+
     database.all('SELECT * FROM pins;').then(pins => {
 
         let pinArray = []
@@ -46,8 +47,10 @@ app.get('/pins', (request, response) => {
 })
 
 app.post('/pins', (request, response) => {
+
     database
-        .run('INSERT INTO pins (pinTitle, pinDescription, pinImage, pinTags, pinCoordinates, pinUser) VALUES (?, ?, ?, ?, ?, ?)', [
+        .run('INSERT INTO pins (pinTitle, pinDescription, pinImage, pinTags, pinCoordinates, pinUser) VALUES (?, ?, ?, ?, ?, ?)', 
+        [
             request.body.pinTitle,
             request.body.pinDescription,
             request.body.pinImage,
@@ -56,42 +59,42 @@ app.post('/pins', (request, response) => {
             request.body.pinUser
         ])
         .then(() => {
+
             response.status(201).send(request.body)
         })
         .catch(error => {
+
             response.send(error)
         })
 })
 
 app.patch('/pins/:pin', (request, response) => {
-    for (let i = 0; i < pins.length; i++) {
-        if (request.params.pin === pins[i].pinId) {
-            if (request.body.title !== undefined) {
-                pins[i].title = request.body.title
-            }
-            if (request.body.description !== undefined) {
-                pins[i].description = request.body.description
-            }
-            if (request.body.image !== undefined) {
-                pins[i].image = request.body.image
-            }
-            if (request.body.tags !== undefined) {
-                pins[i].tags = request.body.tags
-            }
-            if (request.body.coordinates !== undefined) {
-                pins[i].coordinates = request.body.coordinates
-            }
-            if (request.body.distance !== undefined) {
-                pins[i].distance = request.body.distance
-            }
-        }
-    }
-    response.send('Update complete')
+
+    database.all('SELECT * FROM pins WHERE pinId = ?', [request.params.pin])
+            .then((pins) => {
+
+                let updatedPin = Object.assign(pins[0], request.body)
+
+                database.run('UPDATE pins SET pinTitle = ?, pinDescription = ?, pinImage = ?, pinTags = ?, pinCoordinates = ? WHERE pinId = ?',
+                    [
+                        updatedPin.pinTitle, 
+                        updatedPin.pinDescription,
+                        updatedPin.pinImage,
+                        JSON.stringify(updatedPin.pinTags),
+                        JSON.stringify(updatedPin.pinCoordinates),
+                        updatedPin.pinId
+                    ]).then(() => {
+                        
+                        response.send('Pin updated')
+                    })
+            })
 })
 
 app.delete('/pins/:pin', (request, response) => {
+
     database.run('DELETE FROM pins WHERE pinId=?', [request.params.pin])
         .then(() => {
-            response.send('Delete request executed')
+
+            response.send('Pin deleted')
         })
 })
